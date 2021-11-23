55944eeeb63033b0717bfaa96d4bc1f146890ffc
Dominic Flocea <45204549+domeeno@users.noreply.github.com>
2021-11-23 16:49:27 +0200

    foundation for delete node, added node searches

--- bst.c
+++ bst.c
@@ -9,22 +9,32 @@ struct Node {
 };
 
 struct Node* insert(int value, struct Node *root);
-
 struct Node* newNode(int value);
-
 void inorder(struct Node *root);
+int searchNodeValue(int value, struct Node *root);
+struct Node* searchNode(int value, struct Node *root);
+struct Node* searchParentNode(int value, struct Node *root);
+struct Node* deleteNode(struct Node* root, int key);
 
 int main()
 {
-    struct Node root = {10, NULL, NULL};
+    struct Node *root = NULL;
     
-    insert(2, &root);
-    insert(18, &root);
-    insert(13, &root);
-    insert(4, &root);
-    insert(1, &root);
+    root = insert(10, root);
     
-    inorder(&root);
+    insert(2, root);
+    insert(18, root);
+    insert(13, root);
+    insert(4, root);
+    insert(1, root);
+    insert(3, root);
+    insert(23, root);
+    
+    // inorder(root);
+    
+    struct Node *someNode = deleteNode(root, 2);
+    printf("%d", someNode->data);
+    
     
     return 0;
 }
@@ -32,17 +42,15 @@ int main()
 struct Node* insert(int value, struct Node *root) {
     if (root == NULL) {
         root = newNode(value);
-    } else if (root->data > value) {
+    } else if (root->data < value) {
         root->right_child = insert(value, root->right_child);
     } else {
         root->left_child = insert(value, root->left_child);
     }
-    
     return root;
 }
 
-struct Node* newNode(int value)
-{
+struct Node* newNode(int value) {
     struct Node *new_node;
     new_node = malloc(sizeof(struct Node));
     new_node->data = value;
@@ -52,12 +60,57 @@ struct Node* newNode(int value)
     return new_node;
 }
 
+int searchNodeValue(int value, struct Node *root) {
+    if(&root->data == NULL) {
+        return -1;
+    } else if (value > root->data) {
+        searchNode(value, root->right_child);
+    } else if (value < root->data) {
+        searchNode(value, root->left_child);
+    } else if (value == root->data) {
+        return value;
+    }
+}
+
+struct Node* searchNode(int value, struct Node *root) {
+    if(&root->data == NULL) {
+        return root;
+    } else if (value > root->data) {
+        searchNode(value, root->right_child);
+    } else if (value < root->data) {
+        searchNode(value, root->left_child);
+    } else if (value == root->data) {
+        return root;
+    }
+}
+
+struct Node* searchParentNode(int value, struct Node *root) {
+    if (value == root->left_child->data || value == root->right_child->data) {
+        return root;
+    } else if (value > root->data) {
+        searchParentNode(value, root->right_child);
+    } else if (value < root->data) {
+        searchParentNode(value, root->left_child);
+    }
+}
+
+struct Node* deleteNode(struct Node* root, int key){
+    struct Node *parentNode = searchParentNode(key, root);
+    
+    struct Node *nodeToBeDeleted = searchNode(key, parentNode);
+    
+    // TODO make the logic for deletion
+    
+    return nodeToBeDeleted;
+}
+
 void inorder(struct Node *root) {
     if(root!=NULL) // checking if the root is not null
     {
-        inorder(root->right_child); 
+        inorder(root->left_child); 
         printf(" %d ", root->data); 
-        inorder(root->left_child);
+        inorder(root->right_child);
     }
 }
+
 
